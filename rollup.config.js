5ca3d2065b81c12ce341d5302caf3aca37fab1f8
Phil Bayfield <phil@boxes.app>
2024-01-31 04:54:26 +0000

    Update dependencies/configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,15 @@
-import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
+import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
 
 export default {
-  input: 'src/t-writer.js',
-  output: {
-    file: 'dist/t-writer.js',
-    format: 'umd',
-    name: 't-writer'
-  },
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ]
+    input: 'src/t-writer.js',
+    output: {
+        file: 'dist/t-writer.js',
+        format: 'umd',
+        name: 't-writer'
+    },
+    plugins: [
+        nodeResolve(),
+        babel({ babelHelpers: 'external' })
+    ]
 }
